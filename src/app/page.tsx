969d7a2e55d45f8cff0361f439e3190a39d0a463
alokmishra <alok.230101016@iiitbh.ac.in>
2025-06-21 23:12:39 +0530

    home page corrected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 
-import HeroSection from '@/components/HeroSection'
 import { redirect } from 'next/navigation'
 import React from 'react'
 
-
 const Home = () => {
-  return 
-   <HeroSection />
+  return (
+   redirect("/blogs")
+  )
 }
 
 export default Home
